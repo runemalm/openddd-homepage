490ca6501e801eb048a004d085114db9019d0bbc
David Runemalm <david.runemalm@gmail.com>
2025-03-26 14:12:00 +0100

    Add deploy script.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/openddd-homepage/' : '/',
   server: {
     host: "::",
     port: 8080,
